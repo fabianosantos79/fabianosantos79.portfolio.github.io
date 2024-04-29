7a45b3af2706c1a4e7388938b68b74dfd03f5068
Fabiano <63196450+fabianosantos79@users.noreply.github.com>
2024-04-29 03:24:47 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const mudarTema = document.querySelector('.mudar-tema')
+const switcher = document.querySelector('#flexSwitchCheckDefault');
+
+function mudarTema2() {
+    if (switcher.checked) {
+        alert("Marcou");
+    }
+}
+
+mudarTema2();
